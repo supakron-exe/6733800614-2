7eca6769338afa84e1d1d504596ce79f035e610b
poohlikung <118596123+poohlikung@users.noreply.github.com>
2026-07-15 16:06:43 +0700

    feat: update student info - ID 6733800614 sec 2

---
+++ myproject6733800614/src/main/java/com/example/myproject6733800614/Myproject6733800614Application.java
@@ -0,0 +1,13 @@
+package com.example.myproject6733800614;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject6733800614Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject6733800614Application.class, args);
+	}
+
+}
---
+++ myproject6733800614/src/test/java/com/example/myproject6733800614/Myproject6733800614ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myproject6733800614;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myproject6733800614ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
